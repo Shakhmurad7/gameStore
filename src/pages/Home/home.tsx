c766b9c7897079a31d1f69a42ad03c03c329652a
Shakhmurad <muradb356@gmail.com>
2023-10-21 15:24:35 +0400

    add hero in home page

--- src/pages/Home/home.tsx
+++ src/pages/Home/home.tsx
@@ -9,26 +9,33 @@ function Home() {
             <h1>Discover Digital Art & Collect NFTs</h1>
             <p>NFT marketplace UI created with Anima for Figma. Collect, buy and sell art from more than 20k NFT artists.</p>
             <div className={style['hero-icon']}>
-            <h3>Get Started</h3>
             <BsRocketTakeoffFill/>
+            <h5>Get Started</h5>
             </div>
             <div className={style['hero-page']}>
               <div className={style['hero-block']}>
-                <b>240k+ </b>
+                <h2>240k+ </h2>
                 <p>Total Sale</p>
               </div>
               <div className={style['hero-block']}>
-                <b>100k+ </b>
+                <h2>100k+ </h2>
                 <p>Auctions</p>
               </div>
               <div className={style['hero-block']}>
-                <b>240k+ </b>
+                <h2>240k+ </h2>
                 <p>Artists</p>
               </div>
             </div>
           </div>
           <div className={style['hero-right']}>
             <img src={'./img/Image Placeholder.png'} />
+            <div className={style['hero-right-block']}>
+              <h2>Space Walking</h2>
+              <div className={style['hero-right-block-row']}>
+                  <img src={'./img-icon/Avatar Placeholder.svg'} />
+                  <p>Animakid</p>
+              </div>
+            </div>
           </div>
         </div>
     </PageContainer>
