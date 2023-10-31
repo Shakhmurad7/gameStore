826fdd2696ac42fe1ab57851f392f0471be527b8
Shakhmurad <muradb356@gmail.com>
2023-10-31 23:09:20 +0400

    add connect container

--- src/pages/Connect/connect.tsx
+++ src/pages/Connect/connect.tsx
@@ -1,10 +1,60 @@
 import PageContainer from '../../companents/pageContainer'
 import style from './connect.module.css'
+import {BiCopy} from "react-icons/bi";
+import {AiOutlinePlus} from "react-icons/ai";
 
 function Connect() {
   return (
     <PageContainer>
+        <div className={style['container']}>
+        </div>
+        <div className={style['connect-contaner']}>
+            <div className={style['connect-page-right']}>
+                <img className={style['connect-img']} src={'./img/Avatar Placeholder.png'} />
+                <h1>Shakhmurad</h1>
+                <div className={style['connect-block-row']}>
 
+                    <div className={style['connect-block']}>
+                        <h2>250k+</h2>
+                        <p>Volume</p>
+                    </div>
+
+                    <div className={style['connect-block']}>
+                        <h2>50+</h2>
+                        <p>NFTs Sold</p>
+                    </div>
+
+                    <div className={style['connect-block']}>
+                        <h2>3000+</h2>
+                        <p>Followers</p>
+                    </div>
+
+                </div>
+                <div className={style['connect-page-center']}>
+                    <h2>Bio</h2>
+                    <p>The internet's friendliest designer kid.</p>
+                </div>
+                <div className={style['connect-icon-text']}>
+                    <h2>Links</h2>
+                    <div className={style['connect-icon']}>
+                        <img src={'./img-icon/DiscordLogo.svg'}/>
+                        <img src={'./img-icon/InstagramLogo.svg'} />
+                        <img src={'./img-icon/TwitterLogo.svg'} />
+                        <img src={'./img-icon/YoutubeLogo.svg'} />
+                    </div>
+                </div>
+            </div>
+            <div className={style['connect-page-left']}>
+                    <div className={style['connect-block-1-left']}>
+                        <BiCopy/>
+                        <p>0xc0E3...B79C</p>
+                    </div>
+                    <div className={style['connect-block-2-left']}>
+                        <AiOutlinePlus/>
+                        <p>Follow</p>
+                    </div>
+                </div>
+        </div>
     </PageContainer>
   )
 }
