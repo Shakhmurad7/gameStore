3774ed43cad30c9990037780ce27910505c8897e
Shakhmurad <muradb356@gmail.com>
2023-11-04 15:41:49 +0400

    add form in sing-up

--- src/pages/Sing-up/singUP.tsx
+++ src/pages/Sing-up/singUP.tsx
@@ -3,8 +3,25 @@ import style from './sing.module.css'
 import { BiUser } from "react-icons/bi";
 import { AiOutlineMail } from "react-icons/ai";
 import { RiLockPasswordLine } from "react-icons/ri";
-
+import { useState } from "react";
 function SingUP() {
+  const [form , setform] = useState({name:'',password1:'',email:'' , password2:''})
+  
+  const submit = (e:any)=>{
+  e.preventDefault()
+    console.log(form);
+    setform({
+      name:'',
+      password1:'',
+      password2:'',
+      email:''
+    })
+  }
+
+  const handle =(e:any)=>{
+    const {name , value} = e.target
+    setform({...form , [name]:value})
+  }
   return (
     <PageContainer>
         <div className={style['contaner-sing']}>
@@ -14,30 +31,30 @@ function SingUP() {
           <div className={style['sing-text']}>
               <h1>Create account</h1>
               <p>Welcome! enter your details and start creating, collecting and selling NFTs.</p>
+              <form onSubmit={submit}>
               <div className={style['sing-input']}>
-
                 <div className={style['input-block-1']}>
-                  <input type="text"  placeholder="Username" />
+                  <input type="text"   name="name" value={form.name} onChange={handle} placeholder="Username" />
                   <div className={style['BiUser']}><BiUser/></div>
                 </div>
 
                 <div className={style['input-block-1']}>
-                  <input type="email" placeholder="Email Address"  />
+                  <input type="email"  name="email" value={form.email} onChange={handle} placeholder="Email Address"  />
                   <div className={style['AiOutlineMail']}> <AiOutlineMail/></div>
                 </div>
 
                 <div className={style['input-block-1']}>
-                  <input type="password"  placeholder="Password" />
+                  <input type="password"  name="password1" value={form.password1} onChange={handle} placeholder="Password" />
                   <div className={style['RiLockPasswordLine']}> <RiLockPasswordLine/></div>
                 </div>
 
                 <div className={style['input-block-1']}>
-                  <input type="password"  placeholder="Confirm Password" />
+                  <input type="password" name="password2" value={form.password2} onChange={handle} placeholder="Confirm Password" />
                   <div className={style['RiLockPasswordLine']}> <RiLockPasswordLine/></div>
                 </div>
-
-                <button>Create account</button>
+                <button type="submit" >Create account</button>
               </div>
+              </form>
           </div>
         </div>
     </PageContainer>
